a39be90dca726832a45d479bf16916b4c8417f86
imefistoff <azureus325@gmail.com>
2020-07-13 15:26:39 +0300

    Structura

---
+++ app/src/main/java/com/example/sportsclubmanagement/screens/guest/GuestActivity.java
@@ -0,0 +1,16 @@
+package com.example.sportsclubmanagement.screens.guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sportsclubmanagement.R;
+
+public class GuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest);
+    }
+}
