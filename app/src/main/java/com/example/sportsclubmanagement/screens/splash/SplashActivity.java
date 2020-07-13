3fec46d8ad158c3214ae720ec9ea4f9284bb3a37
Razvan <prundel.razvan@gmail.com>
2020-07-13 17:21:50 +0300

    Splash Screen Logic

--- app/src/main/java/com/example/sportsclubmanagement/screens/splash/SplashActivity.java
+++ app/src/main/java/com/example/sportsclubmanagement/screens/splash/SplashActivity.java
@@ -2,15 +2,28 @@ package com.example.sportsclubmanagement.screens.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.sportsclubmanagement.R;
+import com.example.sportsclubmanagement.screens.guest.GuestActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int Splash_Time_Out = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent guestActivity = new Intent(SplashActivity.this, GuestActivity.class);
+                startActivity(guestActivity);
+                finish();
+            }
+        }, Splash_Time_Out);
     }
 }
