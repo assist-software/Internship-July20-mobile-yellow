b060e655b007819fd6f0c2a4246cc6bd5a70f1f9
Razvan <prundel.razvan@gmail.com>
2020-07-15 16:47:39 +0300

    Added UI for Home Screen Bottom Navigation View

--- app/src/main/java/com/example/sportsclubmanagement/screens/main/MainActivity.java
+++ app/src/main/java/com/example/sportsclubmanagement/screens/main/MainActivity.java
@@ -7,12 +7,19 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.sportsclubmanagement.R;
+import com.example.sportsclubmanagement.fragments.clubs.ClubsFragment;
+import com.example.sportsclubmanagement.fragments.events.EventsFragment;
+import com.example.sportsclubmanagement.fragments.home.HomeFragment;
+import com.example.sportsclubmanagement.fragments.workouts.WorkoutsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView homeNavigationView;
     private Toolbar toolbar;
     private ActionBarDrawerToggle homeDrawerToggle;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,15 +57,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void initComponents() {
         toolbar = findViewById(R.id.home_tool_bar);
         drawerLayout = findViewById(R.id.home_drawer_layout);
-        homeNavigationView = findViewById(R.id._home_nav_view);
+        homeNavigationView = findViewById(R.id.home_nav_view);
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
         homeDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         homeDrawerToggle.getDrawerArrowDrawable().setColor(ContextCompat.getColor(this, R.color.colorWhite));
         drawerLayout.addDrawerListener(homeDrawerToggle);
         homeDrawerToggle.syncState();
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); //remove the default title from the action bar
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit(); //show home fragment first time you enter the activity
         homeNavigationView.setNavigationItemSelectedListener(this);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.bottom_nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.bottom_nav_clubs:
+                            selectedFragment = new ClubsFragment();
+                            break;
+                        case R.id.bottom_nav_events:
+                            selectedFragment = new EventsFragment();
+                            break;
+                        case R.id.bottom_nav_workouts:
+                            selectedFragment = new WorkoutsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 
     //In switch-ul de mai jos adaugam functionalitatea pentru fiecare buton din meniul lateral din home activity
     @Override
